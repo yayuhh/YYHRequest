0bf82faff4acafd89128782b84241804a8dc58e0
Angelo Di Paolo <angelod101@gmail.com>
2014-03-19 08:40:50 -0400

    add some tests for YYHRequest

--- YYHRequestTests/YYHRequestTests.m
+++ YYHRequestTests/YYHRequestTests.m
@@ -8,27 +8,32 @@
 
 #import <XCTest/XCTest.h>
 
+#import "YYHRequest.h"
+
 @interface YYHRequestTests : XCTestCase
 
 @end
 
 @implementation YYHRequestTests
 
-- (void)setUp
-{
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+- (void)testDefaultProperties {
+    NSURL *url = [NSURL URLWithString:@"http://www.google.com/"];
+    YYHRequest *request = [YYHRequest requestWithURL:url];
+    
+    XCTAssertTrue([request.method isEqualToString:@"GET"]);
+    XCTAssertTrue(request.completeOnMainThread);
+    XCTAssertEqual(request.url, url);
 }
 
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+- (void)testSetHeaderProperties {
+    NSURL *url = [NSURL URLWithString:@"http://www.google.com/"];
+    YYHRequest *request = [YYHRequest requestWithURL:url];
+    
+    request.contentType = @"text/plain";
+    request.userAgent = @"some test agent";
+    
+    XCTAssertEqual(request.headers[@"Content-Type"], request.contentType);
+    XCTAssertEqual(request.headers[@"User-Agent"], request.userAgent);
 }
 
 @end
